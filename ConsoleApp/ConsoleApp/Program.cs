5cfe61f8a646dc8cdaad81e46c9e88bfb84f50b9
Roman Moslakov <mailtoron@yandex.ru>
2021-03-31 14:23:29 +0300

    Singletone pattern

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SingletonClass1 temp1 = SingletonClass1.SingletonGetInstanse();
+            SingletonClass1 temp2 = SingletonClass1.SingletonGetInstanse();
+
+            if (temp1 == temp2)
+            {
+                Console.WriteLine("All works well");
+            }
+        }
+    }
+}
---
+++ ConsoleApp/ConsoleApp/SingletonClass1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{ 
+    class SingletonClass1
+    {
+        private SingletonClass1() { }
+
+        private static SingletonClass1 _singletonInstanse;
+
+        public static SingletonClass1 SingletonGetInstanse()
+        {
+            if (_singletonInstanse == null)
+            {
+                _singletonInstanse = new SingletonClass1();
+            }
+            return _singletonInstanse;
+        }
+
+        public static void OtherMethod()
+        {
+
+        }
+    }
+}
